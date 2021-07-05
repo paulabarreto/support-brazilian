31f0fc9a642670b14389cd6e5dddfd8fe3f48d7a
PAULA BARRETO <paulavilaca@gmail.com>
2021-07-05 14:12:57 -0400

    Added Auth0 Authentication

---
+++ src/components/signup-button.js
@@ -0,0 +1,20 @@
+// src/components/signup-button.js
+
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const SignupButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <button
+      className="btn btn-primary btn-block"
+      onClick={() =>
+        loginWithRedirect()
+      }
+    >
+      Sign Up
+    </button>
+  );
+};
+
+export default SignupButton;
