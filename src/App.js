571d87a9fd7d5050f41f9e1b6cea4c1bc1f2082f
PAULA BARRETO <paulavilaca@gmail.com>
2021-07-30 10:52:41 -0400

    removed sort by name function

--- src/App.js
+++ src/App.js
@@ -95,8 +95,6 @@ function App() {
           }
         })
       }
-      // list.join(sortByName(list));
-      // list.sort(sortByName(list));
       if(isAdmin) {
         list.sort(showPendingApprovalFirst(list));
       }
@@ -118,22 +116,6 @@ function App() {
       }
       return 0
     })
-  }
-
-  const sortByName = (list) => {
-    list.sort(function(a, b) {
-      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-  
-      // names must be equal
-      return 0;
-    });
   }
 
   const getFavouritesList = async (user) => {
