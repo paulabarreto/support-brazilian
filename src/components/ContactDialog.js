f62d2cabcfde34f971560b520ce12c5875a8b966
PAULA BARRETO <paulavilaca@gmail.com>
2022-04-27 20:32:57 -0400

    fixed contact dialog bug

--- src/components/ContactDialog.js
+++ src/components/ContactDialog.js
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-export default function ContactDialog(props) {
+export default function FormDialog(props) {
 
   return (
     <div>
-      <Dialog open={props.open} onClose={props.handleClose}>
-        <DialogTitle>Contact</DialogTitle>
+      <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
           </DialogContentText>
           <TextField
             autoFocus
@@ -25,12 +25,15 @@ export default function ContactDialog(props) {
             label="Email Address"
             type="email"
             fullWidth
-            variant="standard"
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={props.handleClose}>Cancel</Button>
-          <Button onClick={props.handleClose}>Subscribe</Button>
+          <Button onClick={props.handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={props.handleClose} color="primary">
+            Subscribe
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
